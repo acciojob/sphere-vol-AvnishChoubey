74c894618899c6043e27ff79950abce65026c4c9
Avnish Choubey <118668098+AvnishChoubey@users.noreply.github.com>
2025-10-08 15:59:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function volume_sphere() {
     //Write your code here
-	const radius = parseFloat(document.getElementById("radius").value);
-	if(!isNaN(radius) && radius > 0) {
-		const volume = (4/3)*Math.PI*Math.pow(radius, 3);
-		const submitBtn = document.getElementById("submit");
-		submitBtn.addEventListener('click', function() {
+	const submitBtn = document.getElementById("submit");
+	submitBtn.addEventListener('click', function(event) {
+		event.preventDefault();
+		
+		const radius = parseFloat(document.getElementById("radius").value);
+		
+		if(!isNaN(radius) && radius >= 0) {
+			const volume = (4/3)*Math.PI*Math.pow(radius, 3);
 			document.getElementById('volume').value = volume.toFixed(4);
-		});
-	} else {
-		document.getElementById('volume').value = NaN;
-	}
+		} else {
+			document.getElementById('volume').value = null;
+		}
+	});
 }
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
